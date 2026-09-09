dc8c4719b34e343a581c5c142447230a6b032a28
Vikramsahani05 <vikramsahani9741@gmail.com>
2026-09-09 09:47:30 +0530

    day 1

---
+++ student task management-portal/src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+function welcome() {
+  return (
+    <div className="welcome-container">
+      <h1>Welcome to the Student Task Management Portal</h1>
+
+    </div>
+  );
+}
+export default welcome;
